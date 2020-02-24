df78a0d6ef0365353981f3cbded0c7372ef864d6
Uryel Constancio <uryelhenrique.c@gmail.com>
2020-02-24 22:34:25 +0000

    Add doc

--- app/src/main/java/com/uhc/shapescreator/editor/ShapeView.kt
+++ app/src/main/java/com/uhc/shapescreator/editor/ShapeView.kt
@@ -16,18 +16,27 @@ class ShapeView(context: Context?, attrs: AttributeSet?) :
 
     private lateinit var modelsList: List<ShapeModel>
 
+    /**
+     * Setup paint colour for each shape
+     */
     private fun setupPaint() {
         drawSquarePaint.color = Color.BLUE
         drawCirclePaint.color = Color.GREEN
         drawTrianglePaint.color = Color.RED
-        drawTrianglePaint.style = Paint.Style.FILL
-        drawTrianglePaint.isAntiAlias = true
     }
 
+    /**
+     * Setup paint on init
+     */
     init {
         setupPaint()
     }
 
+    /**
+     * Draws the UI for the view on the provided [Canvas].
+     *
+     * This method does nothing if a [ShapeModel] has not been provided by the view model.
+     */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
@@ -36,9 +45,8 @@ class ShapeView(context: Context?, attrs: AttributeSet?) :
                 shapeModel.apply {
                     when (shapeType) {
                         /** Square */
-                        ShapeType.SQUARE -> {
-                            canvas.drawRect(toRectF(), drawSquarePaint)
-                        }
+                        ShapeType.SQUARE -> canvas.drawRect(toRectF(), drawSquarePaint)
+
                         /** Circle */
                         ShapeType.CIRCLE -> canvas.drawCircle(
                             centreX,
@@ -46,6 +54,7 @@ class ShapeView(context: Context?, attrs: AttributeSet?) :
                             SHAPE_WIDTH,
                             drawCirclePaint
                         )
+
                         /** Triangle */
                         ShapeType.TRIANGLE -> {
                             /** Top */
@@ -67,11 +76,19 @@ class ShapeView(context: Context?, attrs: AttributeSet?) :
         }
     }
 
+    /**
+     * Update to display the provided [ShapeModel].
+     *
+     * This method calls [View.invalidate] to force the view to be redrawn using [View.onDraw].
+     */
     fun update(modelsList: List<ShapeModel>) {
         this.modelsList = modelsList
         invalidate()
     }
 
+    /**
+     * Convert a [ShapeModel] to a [RectF] so it can draw a Square
+     */
     private fun ShapeModel.toRectF() = RectF(
         centreX,
         centreX,
@@ -80,6 +97,7 @@ class ShapeView(context: Context?, attrs: AttributeSet?) :
     )
 
     companion object {
+        /** Shape width being used for all the shapes */
         private const val SHAPE_WIDTH = 150f
     }
 }
